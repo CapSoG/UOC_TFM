bd287ad3ae189e6c4d0ffa30872df63bd3e68c1a
CapSoG <50954266+CapSoG@users.noreply.github.com>
2022-04-04 00:14:07 +0200

    Update ethIoT.sol

--- ethIoT.sol
+++ ethIoT.sol
@@ -1,1 +1,25 @@
+contract lightSensor {
+  bool LEDturnedOn;
+  bool currentState;
+  address owner;
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  // Constructor
+  function isLightTurnedOn() public constant returns(bool){
+    return LEDturnedOn;
+  }
+
+  function turnLightOn() public payable{
+    if(msg.value < 1000 ){revert();}
+    LEDturnedOn = true;
+  }
+  function turnLigthOff() public payable{
+    LEDturnedOn = false;
+  }
+
+}
 
